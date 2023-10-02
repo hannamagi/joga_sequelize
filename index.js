9487bd321ac75ba6d5cf73d50a738c5632955e5c
hannamagi <hanna.magi14@gmail.com>
2023-10-02 22:10:00 +0300

    Created connection to database

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+
+app.use(express.urlencoded({ extended: true }));
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize ('mysql://root:qwerty@localhost:3306/joga_sequelize')
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connected to the database.');
+    })
+    .catch(err=> {
+        console.error('Unable to connect to the database:', err);
+    })
 
 app.get("/", (req, res) => {
-    res.json({message: "Welcome to the sequelize application."})
-});
+    res.json({ message: "Welcome to sequelize application." });
 
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
+    console.log('Server is running on http://localhost:3000');
 });
